103b60d49d72b3b9fe9179ec2e3c92da4121b34b
EXIA <1755069698@qq.ocm>
2020-02-25 11:50:32 +0800

    fix pypi's readme

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r",encoding='utf8') as fh:
     long_description = fh.read()
 
 setup(
     name = 'hyanova',
-    version = '1.0.4',
+    version = '1.0.9',
     keywords = ('anova', 'sklearn','hyperparameter','hyperparameter importance'),
     description = 'A pure python implementation of fuctional ANOVA algorithm.',
     license = 'MIT',
@@ -16,7 +16,7 @@ setup(
     url = 'https://github.com/exiarepairii/hyanova',
     packages = find_packages(),
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     platforms = 'any',
     python_requires='>=3.6',
 )
